b47565d8e2faeaa43bdd0d57e8a324c5f15ee60c
je355 <chaeeehui@gmail.com>
2024-07-24 19:18:28 +0900

    feat: markdown button

--- src/components/wirte/index.style.ts
+++ src/components/wirte/index.style.ts
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 
-
 export const Container = styled.div`
     width: 899px;
     height: 710px;
@@ -13,14 +12,63 @@ export const Container = styled.div`
     display: flex;
 `;
 
-export const TextArea = styled.textarea`
+export const EditorContainer = styled.div`
     width: 50%;
     height: 100%;
-    padding: 10px;
-    border: 1px solid #ccc;
+    display: flex;
+    flex-direction: column;
+    padding: 10px; 
+    box-sizing: border-box;
+`;
+
+export const TitleInput = styled.input`
+    color: var(--Neutral-60, #858585);
+    font-family: Pretendard;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 20px; 
+    margin-bottom: 10px; 
+`;
+
+export const ButtonContainer = styled.div`
+    margin: 10px 0; 
+    display: flex;
+    flex-wrap: wrap;
+    gap: 5px;
+`;
+
+export const MarkdownButton = styled.button`
+    color: var(--DarkGray-70, #8E8E8E);
+    font-family: Pretendard;
+    font-size: 10px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 20px; /* 200% */
+    border: none;
+    background: none;
+    padding: 5px 10px;
+    cursor: pointer;
+    flex: 1 1 auto; 
+    
+    &:hover {
+        background: var(--DarkGray-95, #EEE);
+        color: black;
+    }
+`;
+
+export const TextArea = styled.textarea`
+    width: 100%;
+    height: calc(100% - 60px); 
     box-sizing: border-box; 
     resize: none;
     overflow-y: auto;
+    color: var(--Neutral-60, #858585);
+    font-family: Pretendard;
+    font-size: 13px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 20px;
 `;
 
 export const Preview = styled.div`
@@ -30,4 +78,9 @@ export const Preview = styled.div`
     border-left: 1px solid #ccc;
     box-sizing: border-box; 
     overflow-y: auto;
+
+    h1 {
+        font-size: 24px;
+        margin-bottom: 10px;
+    }
 `;
--- src/components/wirte/index.tsx
+++ src/components/wirte/index.tsx
@@ -1,18 +1,70 @@
 import React, { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
-import * as S from "src/components/wirte/index.style"; 
+import * as S from "src/components/wirte/index.style";
 
 const MarkdownEditor: React.FC = () => {
+    const [title, setTitle] = useState<string>('');
     const [markdownText, setMarkdownText] = useState<string>('');
+
+    const insertMarkdown = (syntax: string) => {
+        const textArea = document.getElementById('markdown-textarea') as HTMLTextAreaElement;
+        const start = textArea.selectionStart;
+        const end = textArea.selectionEnd;
+        const before = markdownText.substring(0, start);
+        const after = markdownText.substring(end);
+        const selectedText = markdownText.substring(start, end);
+
+        if (syntax === '**') {
+            setMarkdownText(before + syntax + selectedText + syntax + after);
+        } else if (syntax === '_') {
+            setMarkdownText(before + syntax + selectedText + syntax + after);
+        } else if (syntax === '~~') {
+            setMarkdownText(before + syntax + selectedText + syntax + after);
+        } else if (syntax === '> ') {
+            setMarkdownText(before + syntax + selectedText + after);
+        } else if (syntax === '![]()') {
+            setMarkdownText(before + syntax + after);
+        } else if (syntax === '```') {
+            setMarkdownText(before + syntax + '\n' + selectedText + '\n' + syntax + after);
+        } else {
+            setMarkdownText(before + syntax + selectedText + after);
+        }
+
+        setTimeout(() => {
+            textArea.selectionStart = textArea.selectionEnd = start + syntax.length;
+            textArea.focus();
+        }, 0);
+    };
 
     return (
         <S.Container>
-            <S.TextArea
-                value={markdownText}
-                onChange={(e) => setMarkdownText(e.target.value)}
-                placeholder="내용을 적어주세요"
-            />
+            <S.EditorContainer>
+                <S.TitleInput
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    placeholder="제목을 입력하세요"
+                />
+                <S.ButtonContainer>
+                    <S.MarkdownButton onClick={() => insertMarkdown('# ')}>H1</S.MarkdownButton>
+                    <S.MarkdownButton onClick={() => insertMarkdown('## ')}>H2</S.MarkdownButton>
+                    <S.MarkdownButton onClick={() => insertMarkdown('### ')}>H3</S.MarkdownButton>
+                    <S.MarkdownButton onClick={() => insertMarkdown('#### ')}>H4</S.MarkdownButton>
+                    <S.MarkdownButton onClick={() => insertMarkdown('**')}>B</S.MarkdownButton>
+                    <S.MarkdownButton onClick={() => insertMarkdown('_')}>i</S.MarkdownButton>
+                    <S.MarkdownButton onClick={() => insertMarkdown('~~')}>s</S.MarkdownButton>
+                    <S.MarkdownButton onClick={() => insertMarkdown('> ')}>""</S.MarkdownButton>
+                    <S.MarkdownButton onClick={() => insertMarkdown('![]()')}>img</S.MarkdownButton>
+                    <S.MarkdownButton onClick={() => insertMarkdown('```')}>c</S.MarkdownButton>
+                </S.ButtonContainer>
+                <S.TextArea
+                    id="markdown-textarea"
+                    value={markdownText}
+                    onChange={(e) => setMarkdownText(e.target.value)}
+                    placeholder="내용을 입력하세요"
+                />
+            </S.EditorContainer>
             <S.Preview>
+                <h1>{title}</h1>
                 <ReactMarkdown>{markdownText}</ReactMarkdown>
             </S.Preview>
         </S.Container>
