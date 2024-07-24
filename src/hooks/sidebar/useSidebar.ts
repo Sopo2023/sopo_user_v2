f09809a15d9f91adbc07ed2cb3a5381f2760e6ef
je355 <chaeeehui@gmail.com>
2024-07-24 11:25:05 +0900

    fix: hook을 분리해보앗어용

---
+++ src/hooks/sidebar/useSidebar.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const useSidebar = () => {
+  const [activeItem, setActiveItem] = useState<string | null>(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    const path = location.pathname;
+    if (path === '/main') {
+      setActiveItem('home');
+    } else if (path === '/seniortojunior') {
+      setActiveItem('mentor');
+    } else if (path.startsWith('/portfolio')) {
+      setActiveItem('portfolio');
+    } else if (path.startsWith('/competition')) {
+      setActiveItem('competition');
+    } else {
+      setActiveItem(null);
+    }
+  }, [location.pathname]);
+
+  return activeItem;
+};
+
+export default useSidebar;
