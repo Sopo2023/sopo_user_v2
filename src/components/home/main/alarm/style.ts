621beff87ccfe63a90ebba8c78e65e4adae23b3f
해준 <127281057+ftery0@users.noreply.github.com>
2024-08-10 22:00:05 +0900

    feat :: auth 예외처리

--- src/components/home/main/alarm/style.ts
+++ src/components/home/main/alarm/style.ts
@@ -103,3 +103,11 @@ export const ApprovalButton = styled.button`
    font-weight: 900;
   }
 `;
+export const nullBox = styled.div`
+width: 100%;
+height: 100%;
+display: flex;
+align-items: center;
+justify-content: center;
+cursor: pointer;
+`
