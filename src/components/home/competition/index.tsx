2233bbfc72d3e9d2b1985697aac9cff633afeebc
ftery0 <ftery030215@naver.com>
2024-08-22 23:41:57 +0900

    feat :: Suspense 적용

--- src/components/home/competition/index.tsx
+++ src/components/home/competition/index.tsx
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { Suspense } from "react";
 import * as S from "./style";
 import { useGetcompetitionList } from "src/queries/competition/competition.query";
 import PostItem from "src/components/post/competition";
 import PostHeader from "src/components/common/postHeader";
+import ErrorBoundary from "src/components/common/ErrorBoundary";
+import SkeletonComponent from "src/components/common/skeleton";
 const Competition=()=>{
     const  pageParms={
         page:1,
         size:6
     }
-    const {data} = useGetcompetitionList(pageParms);
+    const {data} = useGetcompetitionList(pageParms );
 
     return(
         <S.Container>
         <S.competitionVeiw>
             <PostHeader/>
             <S.PostsGrid>
+                <ErrorBoundary fallback={<><h2>데이터를 불러오지 못했습니다.</h2></>}>
+                <Suspense fallback={<SkeletonComponent height={100}/>}>
+
                 {
                     data?.data.map((post, index)=>(
                         <PostItem
@@ -27,6 +32,8 @@ const Competition=()=>{
                         />
                     ))
                 }
+                </Suspense>
+                </ErrorBoundary>
             </S.PostsGrid>
         </S.competitionVeiw>
         </S.Container>
